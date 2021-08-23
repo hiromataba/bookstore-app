f613b3a0d5d46546f98efcc062110247e8c4837e
Hiro Mataba <aganzemataba@gmail.com>
2021-08-23 20:56:04 +0200

    Add book

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import PropTypes from 'prop-types';
+import Progress from './BookProgress';
+import ActionButtons from './ActionButtons';
 
-const Book = () => {
+const Book = ({ book }) => {
+  const {
+    id, title, category, author, progress,
+  } = book;
+
   return (
     <div>
-      
+      <div>
+        <div>
+          <h4>{category}</h4>
+          <h2>{title}</h2>
+          <h6>{author}</h6>
+        </div>
+        <Progress progress={progress} />
+      </div>
+      <ActionButtons id={id} />
     </div>
-  )
-}
+  );
+};
 
-export default Book
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+    progress: Progress.propTypes.progress,
+  }).isRequired,
+};
+
+export default Book;
