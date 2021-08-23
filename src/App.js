1122079288b534bf3b6b737367369ad287cb9ecb
Hiro Mataba <aganzemataba@gmail.com>
2021-08-23 20:55:09 +0200

    Add books

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+const App = () => (
+  <div>
+    <Nav />
+  </div>
+);
+
+export default App;
---
+++ src/components/books/Book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Book
