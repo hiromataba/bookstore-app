8fb699c2ecbfba7a676af4601aa9100b0c5b56dd
Hiro Mataba <aganzemataba@gmail.com>
2021-08-25 14:26:24 +0200

    Update Book.jsx

--- src/components/books/Book.jsx
+++ src/components/books/Book.jsx
@@ -4,7 +4,7 @@ import ActionButtons from './ActionButtons';
 
 const Book = ({ book }) => {
   const {
-    id, title, category, author, progress,
+    item_id: id, title, category, author, progress,
   } = book;
 
   return (
@@ -12,7 +12,7 @@ const Book = ({ book }) => {
       <div>
         <div>
           <h4>{category}</h4>
-          <h3>{title}</h3>
+          <h2>{title}</h2>
           <h6>{author}</h6>
         </div>
         <Progress progress={progress} />
@@ -24,7 +24,7 @@ const Book = ({ book }) => {
 
 Book.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     author: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
